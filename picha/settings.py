201c29c243722399424db1f84799785fa6954092
jian zhang <jian.zhang@fuwo.com>
2017-04-14 16:14:20 +0800

    modify settings and add two config files

--- picha/settings.py
+++ picha/settings.py
@@ -111,16 +111,5 @@ STATICFILES_DIRS = (
 STATIC_ROOT = '/static/'
 STATIC_URL = '/static/'
 
-# CELERY
-BROKER_URL = 'redis://localhost:6379'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379'
-CELERY_ACCEPT_CONTENT = ['application/json']
-CELERY_TASK_SERIALIZER = 'json'
-CELERY_RESULT_SERIALIZER = 'json'
-CELERY_TIMEZONE = 'Africa/Nairobi'
-
-# Emails
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-EMAIL_HOST = 'localhost'
-EMAIL_PORT = 1025
-DEFAULT_FROM_EMAIL = 'Picha <picha@example.com>'
+from celeryconfig import *
+from config_mail import *
